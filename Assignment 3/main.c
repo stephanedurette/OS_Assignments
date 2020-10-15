922cec20c967fb462d763f050e8c93889a8870e9
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2020-10-14 20:15:54 -0400

    f

--- Assignment 3/main.c
+++ Assignment 3/main.c
@@ -2,18 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//https://man7.org/linux/man-pages/man1/hexdump.1.html
-
-void HexDump(char* filePath, long int maxBytes){
-
-}
 
 void main(int argc, char **argv){
 	char path[] = "/dev/urandom";
-	int numBytes = 160;
-
-	char cmdString[256];
-	sprintf(cmdString, "hexdump -C -n %d %s ", numBytes,path);
-	system(cmdString);
+	printf("%s", "hi");
 }
 
---
+++ Assignment 3/main.h
@@ -0,0 +1,1 @@
+#include "hexDump.h"
