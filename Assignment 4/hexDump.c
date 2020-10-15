c457dd2cfd579a4204147c23bb94f6d29559afba
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2020-10-15 10:57:52 -0400

    works

--- Assignment 4/hexDump.c
+++ Assignment 4/hexDump.c
@@ -1,22 +1,35 @@
 #include "hexDump.h"
 #include <stdio.h>
+#include <string.h>
 
 void hexDump(char* filePath, long unsigned int numBytes, int printASCII){
-	printf("path:\t%s\nnumBytes:\t%lu\nprintASCII:\t%d\n\n", filePath, numBytes, printASCII);
+	printf("path:\t\t%s\nnumBytes:\t%lu\nprintASCII:\t%s\n\n", filePath, numBytes, printASCII ? "true" : "false");
 	
 	FILE *fp = fopen(filePath, "r");
 	if (fp == NULL) return;
 	
-	for(long unsigned int i = 0; i < numBytes; i++){
+	char lineChars[17] ;
+	lineChars[16] = '\0';
+	long unsigned int i;
+	
+	for(i = 0; i < numBytes; i++){
 		char c = fgetc(fp);
 		if (c == EOF) break;
 		
 		if (i % 16 == 0){
-			printf("\n%08ld:\t", i);
+			if(printASCII && i){
+				printf("\t|\t%s", lineChars);
+			}
+			printf("\n%08lX:\t", i);
 		}
-		
-		printf(" %02X", fgetc(fp) & 0xff);
+		lineChars[i % 16] = (c >= ' ' && c<= '~') ? c : '.';
+		printf(" %02X", c & 0xff);
 	}
 	
+	int validChars = i % 16;
+	while(i++ % 16 != 0) printf("   ");
+	if(printASCII) printf("\t|\t%.*s", validChars, lineChars);
+	
+	printf("\n\n");
 	fclose(fp);
 }
