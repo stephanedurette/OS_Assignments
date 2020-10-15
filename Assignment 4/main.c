c074354966d73199d6c82f6d954144bb1ca9fe2e
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2020-10-14 22:07:03 -0400

    f

--- Assignment 4/main.c
+++ Assignment 4/main.c
@@ -20,6 +20,8 @@ void main(int argc, char **argv){
 		case 2:
 			if (!strcmp(argv[1], "-h")){
 				printf("%s", "help!");
+			} else if(!strcmp(argv[1], "-d")){
+				hexDump(DEFAULT_PATH, 160, 1);
 			} else {
 				numBytes = cfileexists(argv[1]) ? 4294967296 : 160;
 				path = cfileexists(argv[1]) ? argv[1] : DEFAULT_PATH;
@@ -27,7 +29,13 @@ void main(int argc, char **argv){
 			}
 		break;
 		default:
-		
+			if (!strcmp(argv[1], "-d")){
+				numBytes = cfileexists(argv[2]) ? 4294967296 : 160;
+				path = cfileexists(argv[2]) ? argv[2] : DEFAULT_PATH;
+				hexDump(path, numBytes, 1);
+			} else {
+				printf("%s", "incorrect format");
+			}
 		
 		break;
 	}
