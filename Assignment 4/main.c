5d91cc39c818c5d58a19c4e0237b304b03e2ca36
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2020-10-14 22:00:51 -0400

    f

--- Assignment 4/main.c
+++ Assignment 4/main.c
@@ -18,9 +18,13 @@ void main(int argc, char **argv){
 			hexDump(DEFAULT_PATH, 160, 0);
 		break;
 		case 2:
-			numBytes = cfileexists(argv[1]) ? 4294967296 : 160;
-			path = cfileexists(argv[1]) ? argv[1] : DEFAULT_PATH;
-			hexDump(path, numBytes, 0);
+			if (!strcmp(argv[1], "-h")){
+				printf("%s", "help!");
+			} else {
+				numBytes = cfileexists(argv[1]) ? 4294967296 : 160;
+				path = cfileexists(argv[1]) ? argv[1] : DEFAULT_PATH;
+				hexDump(path, numBytes, 0);
+			}
 		break;
 		default:
 		
