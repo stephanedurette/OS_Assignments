a23a1f4572e3fae377af8c32294e1199cb4e950e
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2020-10-14 22:13:00 -0400

    f

--- Assignment 4/hexDump.c
+++ Assignment 4/hexDump.c
@@ -2,5 +2,8 @@
 #include <stdio.h>
 
 void hexDump(char* filePath, long unsigned int numBytes, int printASCII){
-	printf("path:\t%s\nnumBytes:\t%lu\nprintASCII:\t%d\n", filePath, numBytes, printASCII);
+	printf("path:\t%s\nnumBytes:\t%lu\nprintASCII:\t%d\n\n", filePath, numBytes, printASCII);
+	
+	
+	
 }
