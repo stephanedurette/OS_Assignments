2d2464d22654b91c9ec551a6c1a496ee6fafbad5
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2020-10-14 22:35:13 -0400

    f

--- Assignment 4/hexDump.c
+++ Assignment 4/hexDump.c
@@ -4,6 +4,19 @@
 void hexDump(char* filePath, long unsigned int numBytes, int printASCII){
 	printf("path:\t%s\nnumBytes:\t%lu\nprintASCII:\t%d\n\n", filePath, numBytes, printASCII);
 	
+	FILE *fp = fopen(filePath, "r");
+	if (fp == NULL) return;
 	
+	for(long unsigned int i = 0; i < numBytes; i++){
+		char c = fgetc(fp);
+		if (c == EOF) break;
+		
+		if (i % 16 == 0){
+			printf("\n%ld:\t", i);
+		}
+		
+		printf(" %x", fgetc(fp) & 0xff);
+	}
 	
+	fclose(fp);
 }
