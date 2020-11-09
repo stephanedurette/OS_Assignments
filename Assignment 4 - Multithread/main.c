0a15769b8ba104c91e3d5458a9c330ad7795ff0a
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2020-11-09 01:40:57 -0500

    Update main.c

--- Assignment 4 - Multithread/main.c
+++ Assignment 4 - Multithread/main.c
@@ -3,8 +3,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+static int randomNumber = -1;
+pthread_t subjectThread;
+pthread_mutex_t lock;
+
+void* subjectFunction(void *arg){
+	while(1){
+		randomNumber = rand() % 1000 + 1;
+	}
+}
+
 void main(int argc, char **argv){
+	srand(time(NULL));
 	
+	if (pthread_mutex_init(&lock, NULL) != 0){
+		printf("mutex init failed");
+	return 1;
+}
+
 }
 
 
